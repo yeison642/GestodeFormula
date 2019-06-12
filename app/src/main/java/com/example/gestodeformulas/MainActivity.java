569c9a0de5e82bc9b3fbf7bcf2a68833ba176fa7
PROYECTO <yeisonae@unicauca.edu.co>
2019-06-11 23:03:34 -0500

    Creacion categoria Calculo

--- app/src/main/java/com/example/gestodeformulas/MainActivity.java
+++ app/src/main/java/com/example/gestodeformulas/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.gestodeformulas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    String []valores = new String[] {"ALGEBRA","CALCULO","FISICA","QUIMICA","TRIGONOMETRIA","ESTADISTICA"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView)findViewById(R.id.lvLista);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1,valores);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(),"Position"+i,Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/gestodeformulas/calculo.java
@@ -0,0 +1,14 @@
+package com.example.gestodeformulas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class calculo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculo);
+    }
+}
