dd3e09fa6ab10ede2e136cf830e7a1d4985f90d3
PROYECTO <yeisonae@unicauca.edu.co>
2019-06-20 22:41:23 -0500

    se añadir navegaciona Url y Resultado de scan QR

--- app/src/main/java/com/example/gestodeformulas/qr.java
+++ app/src/main/java/com/example/gestodeformulas/qr.java
@@ -2,7 +2,9 @@ package com.example.gestodeformulas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class qr extends AppCompatActivity {
@@ -13,6 +15,11 @@ public class qr extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr);
+    }
+    //Metodo del boton Scann
+    public void scan(View view){
+        Intent scan = new Intent(this, resultadoQR.class);
+        startActivity(scan);
     }
 
 }
---
+++ app/src/main/java/com/example/gestodeformulas/resultadoQR.java
@@ -0,0 +1,14 @@
+package com.example.gestodeformulas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class resultadoQR extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultado_qr);
+    }
+}
